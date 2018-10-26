ca71d52b95388fc30ecc5bb632dae8e97518329d
larma1732570 <larma1732570@etu.cegepjonquiere.ca>
2018-10-25 21:24:38 -0400

    2 ok

--- Exercice JS/exercice 9/JS/traitement.js
+++ Exercice JS/exercice 9/JS/traitement.js
@@ -1,6 +1,26 @@
 function btnCalculer_onclick()
 { 
 //declaration des variables
+ var  nbrEtu, nbr65Ans, NbrNormal, nbrBillet, res, prixBilletEtu, prixBillet65Ans, prixBilletNormal;
+
+   nbrEtu = parseInt(document.getElementById("etu").value);
+            prixBilletEtu = 12*nbrEtu;
+    nbr65Ans = parseInt(document.getElementById("65ans").value);
+            prixBillet65Ans = 15*nbr65Ans;
+    nbrNormal = parseInt(document.getElementById("normal").value);
+            prixBilletNormal = 20*nbrNormal;
+        res = parseInt(document.getElementById("etu").value);
+           if (nbrBillet > 5)
+           {
+               res = (prixBillet65Ans + prixBilletEtu + prixBilletNormal) * (prixBillet65Ans + prixBilletEtu + prixBilletNormal * .1);
+           }
+           else
+           {
+               res = prixBillet65Ans + prixBilletEtu + prixBilletNormal;
+           }
+           //Afichage
+
+
 
 
 
