ab282f4276b731368ca15f68002cdef5cacbfa5e
larma1732570 <larma1732570@etu.cegepjonquiere.ca>
2018-10-26 13:46:02 -0400

    garderie ok

--- Exercice JS/GarderieAnimaux/JS/traitement.js
+++ Exercice JS/GarderieAnimaux/JS/traitement.js
@@ -1,25 +1,25 @@
 function btnCalculer_onclick()
-{ 
- 	var Veterinaire, txtnbreJours, ChkServ, TxtNbreJours, radChien, radChat, LblMessage, btnCalculer, res;
-    ChkServ = parseInt(document.getElementById("ChkServ").value);
- 	         ChkServ = 5;
-    TxtNbreJours = parseInt(document.getElementById("txtNbreJours").value);
-    radChien = document.getElementById("radChien").value;
-    radChat = document.getElementById("radChat").value;
-    if (document.getElementById("Audrey").value)
-    {
-        txtnbreJours = 2;
-        radChien;
-        res = 48.29;
-    }
-    else
-    {
-        txtnbreJours = 4;
-        radChat;
-        res = 77.87;
-    }
 
-
+{
+	var prix, animal, jours, toil;
+ 	if (document.getElementById("lstVeterinaire").value = document.getElementById("Audrey").value)
+	{
+        document.getElementById("txtNbreJours").value = 2;
+        document.getElementById("chkServ").checked = true;
+        document.getElementById("radChien").checked = true;
+        document.getElementById("lblMessage").value = 48.29;
+	}
+    else if (document.getElementById("lstVeterinaire").value = document.getElementById("Maxime").value )
+	{
+        document.getElementById("txtNbreJours").value = 4;
+        document.getElementById("chkServ").checked = false;
+        document.getElementById("radChat").checked = true;
+        document.getElementById("lblMessage").value = 77.87;
+	}
+	else
+	{
+        document.getElementById("chkServ").checked = 5;
+	}
 
 
 
