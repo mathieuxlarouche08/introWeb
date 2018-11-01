cea677297e9ab40d981f6464505ab266a8ff7e75
larma1732570 <larma1732570@etu.cegepjonquiere.ca>
2018-11-01 09:27:02 -0400

    cineclubv1

--- Exercice JS/CinéClub - V1/traitement.js
+++ Exercice JS/CinéClub - V1/traitement.js
@@ -1,13 +1,14 @@
 function btnCalculer_onclick()
 {
     var Representation, Age, Res, PrixBase,Rabais;
+    PrixBase = parseInt(document.getElementById("txtBasePrix").value);
  	if((Representation = document.getElementById("radSoir").checked = true) && (Age <= parseInt(document.getElementById("txtAge").value)))
     {
-        Rabais = 40/100;
+        Rabais = 0.4;
     }
     else if ((Representation = document.getElementById("radMidi").checked = true) && (Age <= parseInt(document.getElementById("txtAge").value)))
     {
-        Rabais = 60/100;
+        Rabais = 0.6;
     }
     else if ((Representation = document.getElementById("radSoir").checked = true ) && (Age > parseInt(document.getElementById("txtAge").value)))
     {
@@ -15,6 +16,8 @@ function btnCalculer_onclick()
     }
     else
     {
-        Rabais = 30/100;
+        Rabais = 0.3;
     }
+    Res = PrixBase * Rabais;
+    console.log("le prix total sera de " + Res)
 }
