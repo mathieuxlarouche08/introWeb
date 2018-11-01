7df0365d4d4d323caa33e8307105e12ba8b1d09a
larma1732570 <larma1732570@etu.cegepjonquiere.ca>
2018-11-01 10:29:06 -0400

    si composé

--- Exercice JS/Exercices -  Si composé/JS/traitement12.js
+++ Exercice JS/Exercices -  Si composé/JS/traitement12.js
@@ -1,5 +1,26 @@
 function btnCalculer_onclick()
 { 
- 	
+ 	var nbr1,nbr2,nbr3,nbr4;
 
+ 	nbr1=parseInt(document.getElementById("txtNbre1").value);
+    nbr2=parseInt(document.getElementById("txtNbre2").value);
+    nbr3=parseInt(document.getElementById("txtNbre3").value);
+    nbr4 = 3;
+    if ((nbr1 >= nbr2) && (nbr2 =! nbr3))
+	{
+		nbr4 = nbr4 + 2 * nbr1;
+	}
+	else
+	{
+		if ((nbr1 = nbr2) && (nbr2 > nbr3))
+		{
+			nbr4 = nbr4 + nbr1;
+		}
+		else
+		{
+			nbr4 = nbr4 * nbr2 / nbr1 - nbr3;
+		}
+	}
+	//afficher
+	document.getElementById("btnCalculer").innerHTML ="Le résultat est" + nbr4;
 }
