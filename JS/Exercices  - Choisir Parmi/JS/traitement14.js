87c0351e4b23eeb5831dee5c8abe7c2d1577f30e
larma1732570 <larma1732570@etu.cegepjonquiere.ca>
2018-11-16 14:32:00 -0500

    calculatriceV1

--- JS/Exercices  - Choisir Parmi/JS/traitement14.js
+++ JS/Exercices  - Choisir Parmi/JS/traitement14.js
@@ -1,12 +1,48 @@
 function btnCalculer_onclick()
 {
-    var nbr1, nbr2, operation, res;
+   if (ValideChampObli() === true)
+   {
+       TraiterInfos();
+   }
+}
+function ValideChampObli()
+{
+    var Valide = false;
+    if ((ValideExist("txtNbre1")===true) && (ValideExist("txtNbre2")===true) && ValideExist("txtOperateur"))
+    {
+        Valide = true;
+    }
+    return Valide;
+}
+function ValideExist(nomId)
+{
+    var Valide;
+    if (document.getElementById(nomId).value==="")
+    {
+        Valide = false;
+        document.getElementById(nomId).style.backgroundColor = "red";
+    }
+    else
+    {
+        Valide = true;
+        document.getElementById(nomId).style.backgroundColor = "white";
+    }
+    return Valide;
+}
+function TraiterInfos()
+{
+   var nbr1, nbr2, operation, res;
 
     nbr1 = parseInt(document.getElementById("txtNbre1").value);
     nbr2 = parseInt(document.getElementById("txtNbre2").value);
     operation = document.getElementById("txtOperateur").value;
+    res = CalculerRes(nbr1,nbr2,operation);
+    document.getElementById("lblMessage").innerHTML = "le resultat est" + res;
 
-
+}
+function CalculerRes(nbr1, nbr2, operation)
+{
+    var res;
     switch (operation)
     {
         case "+":
@@ -22,7 +58,5 @@ function btnCalculer_onclick()
             res = nbr1 / nbr2;
             break;
     }
-    document.getElementById("lblMessage").innerHTML = "Le resultat est:" + res;
-
-
+    return res;
 }
